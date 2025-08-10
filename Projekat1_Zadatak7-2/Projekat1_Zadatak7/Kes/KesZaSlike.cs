afbea5a7654a0b3b61dd8ca1af303e381c54122f
Lucode03 <luka.djordjevic@elfak.rs>
2025-08-10 18:53:37 +0200

    Add files via upload

---
+++ Projekat1_Zadatak7-2/Projekat1_Zadatak7/Kes/KesZaSlike.cs
@@ -0,0 +1,61 @@
+﻿using Kes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server1
+{
+    internal class KesZaSlike
+    {
+        protected readonly OptimizovanLRUKes<string, byte[]> _kes;
+        protected readonly string _rootFolder;
+
+        public KesZaSlike(string rootFolder)
+        {
+            _rootFolder = rootFolder;
+            _kes = new OptimizovanLRUKes<string, byte[]>(1000);
+        }
+
+        virtual public byte[]? PribaviSliku(string nazivFajla)
+        {
+            byte[] podaci;
+            if (_kes.ProbajDaPribavisVrednost(nazivFajla, out podaci))
+                return podaci;
+
+            string? putanjaDoFajla = NadjiFajl(nazivFajla);
+            podaci = putanjaDoFajla == null ? null : File.ReadAllBytes(putanjaDoFajla);
+            _kes.DodajIliAzuriraj(nazivFajla, podaci);
+            return podaci;
+        }
+
+        protected string? NadjiFajl(string fileName)
+        {
+            try
+            {
+                foreach (var file in Directory.EnumerateFiles(_rootFolder, "*", SearchOption.AllDirectories))
+                {
+                    if (Path.GetFileName(file).Equals(fileName, StringComparison.OrdinalIgnoreCase))
+                        return file;
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine($"Desila se greska na serveru u neautorizovanom pristupu fajlu: {ex.Message}");
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine($"Desila se greska na serveru jer je putanja do fajla preguda: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        public void PisiRecnik()
+        {
+            _kes.PisiKes();
+        }
+    }
+}
---
+++ Projekat1_Zadatak7-2/Projekat1_Zadatak7/Kes/ProsireniKesZaSlike.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server1
+{
+    internal class ProsireniKesZaSlike:KesZaSlike
+    {
+        private readonly ConcurrentDictionary<string, EventWaitHandle> _lockoviPretrage;
+
+        public ProsireniKesZaSlike(string rootFolder) : base(rootFolder) 
+        {
+            _lockoviPretrage = new ConcurrentDictionary<string, EventWaitHandle>();
+        }
+
+        public override byte[]? PribaviSliku(string nazivFajla)
+        {
+            // Prvo se pokusava u glavnom kesu
+            if (_kes.ProbajDaPribavisVrednost(nazivFajla, out byte[] podaci))
+                return podaci;
+
+            EventWaitHandle waitHandle = null;
+            bool pravaNit = false;
+
+            try
+            {
+                // Ako postoji vec EventHandle vrati se taj,
+                // ako ne, napravi se novi i postavi se prvaNit na true.
+                waitHandle = _lockoviPretrage.GetOrAdd(nazivFajla, key =>
+                {
+                    pravaNit = true;
+                    return new ManualResetEvent(false);
+                });
+
+                if (!pravaNit)
+                {
+                    // Ako nije u pitanju prva pristigla nit, onda se ona blokira dok prva nit ne zavrsi posao
+                    //Console.WriteLine($"[NEE!]Ja nisam prva nit i ja cekam da prva nit zavrsi za trazenje slike {nazivFajla}");
+                    waitHandle.WaitOne(3000);
+
+                    if (_kes.ProbajDaPribavisVrednost(nazivFajla, out podaci))
+                        return podaci;
+
+                    return null; // Fajl nije pronadjen
+                }
+
+                // Ako je prva nit, ona ce obaviti pretragu
+                //Console.WriteLine($"[URA!]Ja sam prva nit i ja krecem da trazim {nazivFajla}.");
+                string putanjaDoFajla = NadjiFajl(nazivFajla);
+                podaci = putanjaDoFajla != null ? File.ReadAllBytes(putanjaDoFajla) : null;
+
+                // Azuriraj kes
+                _kes.DodajIliAzuriraj(nazivFajla, podaci);
+
+                return podaci;
+            }
+            finally
+            {
+                if (pravaNit)
+                {
+                    // Signalizira se drugim nitima da je pretraga zavrsena
+                    ((ManualResetEvent)waitHandle).Set();
+
+                    // Cisti se privremeni waitHandle
+                    _lockoviPretrage.TryRemove(nazivFajla, out _);
+
+                    waitHandle.Close();
+                }
+            }
+
+        }
+    }
+}
